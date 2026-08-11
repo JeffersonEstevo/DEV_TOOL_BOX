248ba56d47a786b164a65bfbab4b7ee62903c96f
JeffersonEstevo <jefferson.estevo@gmail.com>
2026-08-11 20:40:37 -0300

    Fixing Copy/Paste Replace Word

--- static/js/texto/substituir_palavra.js
+++ static/js/texto/substituir_palavra.js
@@ -1,21 +1,3 @@
-// 1. Isolamos a função de colagem para podermos referenciá-la com precisão
-function tratarColagem(event) {
-    // Cancela o comportamento padrão (evita a duplicação)
-    event.preventDefault();
-    
-    // Pega o texto puro da área de transferência
-    const text = (event.clipboardData || window.clipboardData).getData("text");
-    
-    // Insere o texto exatamente onde o cursor do usuário estava posicionado
-    const start = this.selectionStart;
-    const end = this.selectionEnd;
-    const currentValue = this.value;
-    
-    this.value = currentValue.substring(0, start) + text + currentValue.substring(end);
-    
-    // Reposiciona o cursor logo após o texto inserido
-    this.selectionStart = this.selectionEnd = start + text.length;
-}
 
 function inicializarSubstituidor() {
     const originalTextArea = document.getElementById("original-text");
